ad17ba04a40b4878a72abb0de297a0078b633239
J280617 <J280617@tafe.wa.edu.au>
2024-05-18 17:03:12 +0800

    updated the sad smiley to be BLUE

--- sad.py
+++ sad.py
@@ -1,12 +1,13 @@
+import time
 from smiley import Smiley
 
-
 class Sad(Smiley):
+
     def __init__(self):
-        super().__init__()
+        super().__init__(complexion=self.BLUE)
 
         self.draw_mouth()
-        self.draw_eyes()
+        #self.draw_eyes()
 
     def draw_mouth(self):
         """
@@ -16,11 +17,30 @@ class Sad(Smiley):
         for pixel in mouth:
             self.pixels[pixel] = self.BLANK
 
-    def draw_eyes(self, wide_open=True):
+    def draw_eyes(self, wide_open = True):
         """
         Method that draws the eyes (open or closed) on the standard smiley.
         :param wide_open: True if eyes opened, False otherwise
         """
+        # eyes = [9, 14, 17, 22, 18, 21, 25, 26, 29, 30]
         eyes = [10, 13, 18, 21]
         for pixel in eyes:
-            self.pixels[pixel] = self.BLANK if wide_open else self.YELLOW
+            self.pixels[pixel] = self.BLANK if wide_open else self.my_complexion
+
+    def blink(self, delay=0.25):
+        """
+        Make the sad smiley blink once with a certain delay (in s).
+        This is the implementation of the abstract method from the
+        Blinkable abstract class.
+
+        :param delay: Delay in seconds
+        """
+        self.draw_eyes(wide_open=False)
+        self.show()
+        time.sleep(delay)
+        self.draw_eyes(wide_open=True)
+        self.show()
+
+
+
+
--- smiley.py
+++ smiley.py
@@ -1,18 +1,22 @@
 from sense_hat import SenseHat
 
-
 class Smiley:
     WHITE = (255, 255, 255)
     GREEN = (0, 255, 0)
     RED = (255, 0, 0)
     YELLOW = (255, 255, 0)
     BLANK = (0, 0, 0)
+    BLUE = (0, 0, 255)
 
-    def __init__(self):
+    def __init__(self,complexion=YELLOW):
+        self.my_complexion = complexion
+        self.Y = complexion
+
         # We have encapsulated the SenseHat object
         self.sense_hat = SenseHat()
 
-        Y = self.YELLOW
+
+        Y = self.my_complexion
         O = self.BLANK
         self.pixels = [
             O, Y, Y, Y, Y, Y, Y, O,
@@ -37,3 +41,6 @@ class Smiley:
         Show the smiley on the screen.
         """
         self.sense_hat.set_pixels(self.pixels)
+
+        def complexion(self):
+            return self.my_complexion
